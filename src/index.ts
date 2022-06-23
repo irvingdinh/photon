8a2981e4e0e882e47f681060e88a209a9ebb7898
Irving Dinh <irving.dinh@gmail.com>
2022-06-23 17:32:07 +0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,74 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
   //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const url = new URL(request.url)
+    const src = decodeSourceFromURL(url)
+
+    let response = await caches.default.match(request.clone())
+    if (typeof response !== 'undefined') {
+      return response
+    }
+
+    const res = await fetchSource(src)
+    if (res.status > 299) {
+      return new Response(null, {status: res.status})
+    }
+
+    response = new Response(res.body, {
+      headers: computeResponseHeaders(res.headers),
+      status: res.status,
+    })
+
+    await caches.default.put(request.clone(), response.clone())
+
+    return response;
   },
 };
+
+function decodeSourceFromURL(url: URL): string {
+  const {pathname} = url
+  const srcAsBase64 = pathname.replace(/^\//, '')
+
+  return atob(srcAsBase64)
+}
+
+function fetchSource(src: string): Promise<Response> {
+  return new Promise((resolve, reject) => {
+    fetch(src)
+      .then((res) => {
+        if (res.status > 299) {
+          resolve(new Response(null, {
+            status: 404
+          }))
+
+          return
+        }
+
+        resolve(res)
+      })
+      .catch((err) => {
+        console.error(err)
+
+        resolve(new Response(null, {
+          status: 404
+        }))
+      })
+  })
+}
+
+function computeResponseHeaders(source: Headers): Headers {
+  const headers = new Headers()
+
+  for (let key of source.keys()) {
+    if (['content-type'].indexOf(key) !== -1) {
+      headers.set(key, source.get(key) || '')
+    }
+  }
+
+  headers.set('cache-control', 'public, max-age=31536000')
+
+  return headers
+}
